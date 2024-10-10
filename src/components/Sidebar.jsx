3a3f30d228f10f2fe92750e84ea8ddd5088f372d
Titus210 <tituskiplagat210@gmail.com>
2024-10-10 11:54:11 +0300

    adjusted z-index to stop pushing main content aside

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
     <>
       {/* Sidebar Container */}
       {isOpen && (
-        <div className="relative bg-primary flex flex-col gap-12 items-center text-white h-screen py-4 md:p-5 w-64 md:gap-24 md:w-72 lg:w-80">
+        <div className="fixed top-0 left-0 bg-primary flex flex-col gap-12 items-center text-white h-screen py-4 md:p-5 w-64 md:gap-24 md:w-72 lg:w-80 z-50">
           {/* Close Button - Show only on mobile view */}
           <button 
             onClick={toggleSidebar} 
@@ -40,27 +40,27 @@ const Sidebar = () => {
             <ul className="space-y-4">
               <li className="text-lg font-semibold mb-5">Menu</li>
               <li className="hover:bg-gray-800 px-4 py-3 rounded-full">
-                <Link to="/tickets" className="flex items-center">
+                <Link to="/user/dashboard/tickets" className="flex items-center">
                   <FaTicketAlt className="mr-3" /> Tickets
                 </Link>
               </li>
               <li className="hover:bg-gray-800 px-4 py-3 rounded-full">
-                <Link to="/transactions" className="flex items-center">
+                <Link to="/user/dashboard/transactions" className="flex items-center">
                   <FaMoneyBillAlt className="mr-3" /> Transactions
                 </Link>
               </li>
               <li className="hover:bg-gray-800 px-4 py-3 rounded-full">
-                <Link to="/wallet" className="flex items-center">
+                <Link to="/user/dashboard/wallet" className="flex items-center">
                   <FaWallet className="mr-3" /> Wallet
                 </Link>
               </li>
               <li className="hover:bg-gray-800 px-4 py-3 rounded-full">
-                <Link to="/account" className="flex items-center">
+                <Link to="/user/dashboard/account" className="flex items-center">
                   <FaUser className="mr-3" /> Account
                 </Link>
               </li>
               <li className="hover:bg-gray-800 px-4 py-3 rounded-full">
-                <Link to="/logout" className="flex items-center">
+                <Link to="/user/dashboard/logout" className="flex items-center">
                   <FaSignOutAlt className="mr-3" /> Logout
                 </Link>
               </li>
