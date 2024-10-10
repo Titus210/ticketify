06bda5799c46250f506a793a85eede625e4ec9d7
Titus210 <tituskiplagat210@gmail.com>
2024-10-10 23:59:29 +0300

    - removed width declaration and structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import AppRoutes from "./routes.jsx"
+import Navbar from "./components/Navbar.jsx"
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center  mx-auto ">
+    <div className="flex flex-col items-center justify-center w-full mx-auto ">
+      <div className="navbar">
+        {/* <Navbar /> */}
+      </div>
          <AppRoutes />
       </div>
   );
